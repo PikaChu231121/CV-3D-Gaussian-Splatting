3527d507ebe278d6ba7d9bebb7cfe844d19e98a7
PikaChu231121 <18250670351@163.com>
2024-12-04 19:30:58 +0800

    Add files via upload
    CV-3D-Gaussian-Splatting Backend - Spring Boot Project

---
+++ backend/src/main/java/com/cv/gaussianslplatting/backend/Cv3DGaussianSplattingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cv.gaussianslplatting.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cv3DGaussianSplattingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cv3DGaussianSplattingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cv/gaussianslplatting/backend/Cv3DGaussianSplattingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.gaussianslplatting.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv3DGaussianSplattingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
